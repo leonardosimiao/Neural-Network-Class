df17d0a9cfad248c385316c06fdafa73dde5133f
Leonardo Simiao de Luna <leonardosimiaol@gmail.com>
2022-12-11 19:06:51 -0300

    feat: detect people and object intersections

--- tensorflow-human-detection.py
+++ tensorflow-human-detection.py
@@ -15,7 +15,7 @@ TARGET_CLASSES = {  # Classes shown here will be displayed in video
     17: "cat",
     18: "dog",
     27: "backpack",
-    28: "umbrella",
+    # 28: "umbrella",
     # 72: "tv",
     # 73: "laptop",
     # 74: "mouse",
@@ -85,13 +85,9 @@ def calculate_intersection(i, classes_i, boxes_i, data):
     classes = [item[2] for item in data]
 
     ymin_i,xmin_i,ymax_i,xmax_i  = boxes_i[0],boxes_i[1],boxes_i[2],boxes_i[3]
-    # intersection = True 
+    
 
     for ind in range(len(data)):
-        # print(len(data))
-        
-        if ind == i:
-            pass
         
         # ymin,xmin,ymax,xmax = boxes[ind][0],boxes[ind][1],boxes[ind][2],boxes[ind][3]
         boxx = boxes[ind]
@@ -99,70 +95,29 @@ def calculate_intersection(i, classes_i, boxes_i, data):
         xmin = boxx[1] 
         ymax = boxx[2] 
         xmax = boxx[3]
-            #Calculation of the intersections
+
+        #Calculate of the intersections
         if ((xmin_i >= xmax) or (xmax_i <= xmin) or (ymin_i >= ymax) or (ymax_i <= ymin)):
             intersection = False
         else:
             intersection = True
-            # print(xmin, xmax, ymin, ymax)
-            # print(xmin_i, xmax_i, ymin_i, ymax_i)
-            # print(classes[ind])
+            break
 
-        if classes_i == 2:
-            print(f'bike: {xmin_i}, {xmax_i}, {ymin_i}, {ymax_i}')
-            print(intersection)
-            for item in boxes:
-                print(f'{item[1]}, {item[3]}, {item[0]}, {item[2]}')
+        # if classes_i != 1:
+            # print(classes_i)
+            # with open('test_boxes.txt', 'w') as file:
+            #     file.write('bike')
+            #     file.write(f'{xmin_i}, {xmax_i}, {ymin_i}, {ymax_i}')
+            #     file.write('\n')
+            #     file.write(f'Intersection: {intersection} \n')
+            #     for item in boxes:
+            #         file.write(f'{item[1]}, {item[3]}, {item[0]}, {item[2]}')
+            #         file.write('\n')
+            #     file.write('End \n \n \n \n \n')
+            # file.close()
 
-        return intersection
+    return intersection
 
-# def calculate_intersection(i, boxes, classes, num):
-#     #Variable which tells if the hands and objects bounding boxes are being intersected
-
-#     ymin_i,xmin_i,ymax_i,xmax_i  = boxes[i][0],boxes[i][1],boxes[i][2],boxes[i][3]
-#     intersection = False 
-
-#     for ind in range(num):
-        
-#         if ind == i:
-#             pass
-        
-#         ymin,xmin,ymax,xmax = boxes[ind][0],boxes[ind][1],boxes[ind][2],boxes[ind][3]
-
-#             #Calculation of the intersections
-
-#             #1 point of the hand's BB is inserted into the object's BB
-#         if (xmax_i>xmin and xmax_i<xmax and ymin_i < ymax and ymin_i > ymin):
-#             intersection = True
-#         if (xmax_i > xmin and xmax_i < xmax and ymax_i > ymin and ymax_i < ymax):
-#             intersection = True
-#         if (xmin_i < xmax and xmin_i> xmin and ymax_i > ymin and ymax_i < ymax):
-#             intersection = True
-#         if (xmin_i < xmax and xmin_i > xmin and ymin_i < ymax and ymin_i > ymin):
-#             intersection = True
-
-#         #2 points from the object's BB are inserted from the hand's BB
-#         if (xmax_i > xmin and xmax_i < xmax and ymax_i > ymax and ymin_i < ymin):
-#             intersection = True
-#         if (xmin_i < xmin and xmax_i > xmax and ymax_i > ymin and ymax_i < ymax):
-#             intersection = True
-#             print('sim')
-#             if classes[i] == 2:
-#                 print('bike sim')
-#         if (xmin_i < xmax and xmin_i > xmin and ymin_i < ymin and ymax_i > ymax):
-#             intersection = True
-#         if (xmin_i < xmin and xmax_i > xmax and ymax_i > ymax and ymin_i < ymax):
-#             intersection = True
-
-#         #All points of the object's BB are inserted into the hand's BB
-#         if (xmax_i > xmax and xmin_i < xmin and ymax_i > ymax and ymin_i < ymin):
-#             intersection = True
-
-#         #All points of the hand's BB are inserted inside the object's BB
-#         if (xmax_i < xmax and xmin_i > xmin and ymax_i < ymax and ymin_i > ymin):
-#             intersection = True
-
-#         return intersection
 
 if __name__ == "__main__":
     # model_path = 'faster_rcnn_inception_v2_coco_2018_01_28/frozen_inference_graph.pb'
@@ -176,7 +131,7 @@ if __name__ == "__main__":
 
     buffer_human = []
     buffer_object = []
-    BUFFER_SIZE = 100
+    BUFFER_SIZE = 20
 
     sct = mss.mss()
     # count = 1
@@ -198,8 +153,8 @@ if __name__ == "__main__":
         img = cv2.resize(img, (1280, 720))
         boxes, scores, classes, num = odapi.processFrame(img)
 
-        data_human = [item for item in zip(boxes[:num], scores[:num], classes[:num]) if (item[2]==1 and item[1]>threshold/2)]
-        data_object = [item for item in zip(boxes[:num], scores[:num], classes[:num]) if (item[2]!=1 and item[1]>threshold/2)]
+        data_human = [item for item in zip(boxes[:num], scores[:num], classes[:num]) if (item[2]==1 and item[1]>threshold)]
+        data_object = [item for item in zip(boxes[:num], scores[:num], classes[:num]) if ((item[2] in TARGET_CLASSES.keys()) and item[2]!=1 and item[1]>threshold)]
 
         # print(f'num: {num}, human:{len(data_human)}, obj:{len(data_object)} , class:{sum(classes)}')
 
@@ -210,11 +165,6 @@ if __name__ == "__main__":
         buffer_object = data_object + buffer_object
         if len(buffer_object) >= BUFFER_SIZE:
             buffer_object = buffer_object[:BUFFER_SIZE]
-        # for index in range(len(data_full)):
-            # print(data_full[index])
-        # print(f'Comparacao: {boxes[3]}, {scores[3]}, {classes[3]}')
-        # boxes_nonzero = [list_item for list_item in boxes if sum(list_item) != 0]
-        # scores_nonzero = [item for item in scores if item != 0]
 
         # boxes_nonzero = boxes[:num]
 
@@ -226,11 +176,10 @@ if __name__ == "__main__":
             # Changes color of intersecting people and objects
             if classes[i] == 1: # if human
                 doesIntersect = calculate_intersection(i, classes[i], boxes[i], buffer_object) # calculate if intersect with object
-            else:
+            # else:
+            elif classes[i] in TARGET_CLASSES.keys():
                 doesIntersect = calculate_intersection(i, classes[i], boxes[i], buffer_human)
-            # print(f'ymin={boxes[i][0]},xmin={boxes[i][1]},ymax={boxes[i][2]}, xmax = {boxes[i][3]}')
-            # if classes[i] == 2:
-                # print(color)
+
             # Class 1 represents human
             # if classes[i] == 1 and scores[i] > threshold:
             if doesIntersect:
